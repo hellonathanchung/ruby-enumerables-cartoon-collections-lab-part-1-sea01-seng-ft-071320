9699cb7e3cd4d6ecd64d664eecca0870d5c77c15
Nathan Chung <chung.nathanh@gmail.com>
2020-06-22 23:05:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ def list_dwarves(array)
   # Print a numbered list of each element
   total = 0 
   array.each_with_index do |element|
-    puts total + "{#element}"
+    puts total + "${#element}"
 end
